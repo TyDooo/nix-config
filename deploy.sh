ca7b8dbb29e0f2b8c6fbd80f083b0a2cc1eb4953
TyDooo <tydooo@fastmail.com>
2023-12-03 14:41:09 +0100

    add deploy.sh script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+export NIX_SSHOPTS="-tt"
+
+hosts="$1"
+shift
+
+if [ -z "$hosts" ]; then
+    echo "No hosts to deploy"
+    exit 2
+fi
+
+for host in ${hosts//,/ }; do
+    nixos-rebuild --flake .\#"$host" switch --target-host "$host" --use-remote-sudo "$@"
+done
