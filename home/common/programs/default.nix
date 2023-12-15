4dbaa0a85ef77b9f9db460ce8d556e842e9459a8
TyDooo <tydooo@fastmail.com>
2023-12-16 00:53:32 +0100

    temporarily permit unsafe 'electron-25.9.0' pkg

--- home/common/programs/default.nix
+++ home/common/programs/default.nix
@@ -9,6 +9,10 @@
     # ./xdg.nix
   ];
 
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-25.9.0" # FIXME: Necessary for Obsidian, remove after new major update
+  ];
+
   home.packages = with pkgs; [
     vim
     gnumake
