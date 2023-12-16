0f04f70250605d60d611b569aad826034be6d0be
TyDooo <tydooo@fastmail.com>
2023-12-16 14:35:24 +0100

    update spicetify.nix file with new theme and color scheme

--- home/common/programs/spicetify.nix
+++ home/common/programs/spicetify.nix
@@ -6,39 +6,14 @@
   # themable spotify
   programs.spicetify = let
     spicePkgs =
-      inputs.spicetify-nix.packages.${pkgs.hostPlatform.system}.default;
+      inputs.spicetify-nix.packages.${pkgs.system}.default;
   in {
     enable = true;
     injectCss = true;
     replaceColors = true;
     overwriteAssets = true;
     sidebarConfig = true;
-    enabledCustomApps = with spicePkgs.apps; [
-      reddit
-      lyrics-plus
-      new-releases
-    ];
-    theme = spicePkgs.themes.catppuccin-mocha;
-    colorScheme = "pink";
-    enabledExtensions = with spicePkgs.extensions; [
-      fullAppDisplay
-      hidePodcasts
-      shuffle
-      skipStats
-      autoVolume
-      autoSkip
-      savePlaylists
-      phraseToPlaylist
-      wikify
-      autoSkip
-      copyToClipboard
-      history
-      groupSession
-      loopyLoop
-      trashbin
-      bookmark
-      keyboardShortcut
-      fullAppDisplayMod
-    ];
+    theme = spicePkgs.themes.catppuccin;
+    colorScheme = "mocha";
   };
 }
