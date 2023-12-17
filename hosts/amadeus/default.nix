d18ec80d08c88b09bbf21da1c0eea6efc349be6c
TyDooo <tydooo@fastmail.com>
2023-12-17 21:56:47 +0100

    Add mergerfs and file system configurations

--- hosts/amadeus/default.nix
+++ hosts/amadeus/default.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
 
@@ -11,6 +11,43 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "amadeus"; # Define your hostname.
+
+  environment.systemPackages = with pkgs; [
+    mergerfs
+    mergerfs-tools
+  ];
+
+  fileSystems = let
+    storage_disks = {
+      "/mnt/disks/disk0" = "storage0";
+      "/mnt/disks/disk1" = "storage1";
+      "/mnt/disks/disk2" = "storage2";
+      "/mnt/disks/disk3" = "storage3";
+    };
+  in
+    (
+      builtins.mapAttrs
+      (mount_path: label: {
+        fsType = "xfs";
+        device = "/dev/disk/by-label/${label}";
+        options = ["rw" "user" "auto"];
+      })
+      storage_disks
+    )
+    // {
+      "/mnt/cctv" = {
+        fsType = "xfs";
+        device = "/dev/disk/by-label/cctv";
+        options = ["rw" "user" "auto"];
+      };
+
+      # Mount storage
+      "/mnt/storage" = {
+        fsType = "fuse.mergerfs";
+        device = "/mnt/disks/disk0:/mnt/disks/disk1:/mnt/disks/disk2:/mnt/disks/disk3";
+        options = ["cache.files=partial" "dropcacheonclose=true" "category.create=mfs"];
+      };
+    };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
