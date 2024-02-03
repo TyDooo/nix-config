ef9e5a732b5d64e532e0aac9a92dfe3e12c2e7b2
TyDooo <tydooo@fastmail.com>
2024-02-03 16:43:04 +0100

    Add mover package to default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,5 @@
-{pkgs ? import <nixpkgs> {}}: rec {
+{pkgs ? import <nixpkgs> {}}: {
+  mover = pkgs.callPackage ./mover {};
   sf100linux = pkgs.callPackage ./sf100linux.nix {};
   sonarr-v4 = pkgs.callPackage ./sonarr-v4 {};
 }
