bd898349928316580a5ce764e4858795fef8be34
TyDooo <tydooo@fastmail.com>
2024-02-04 12:53:56 +0100

    Update frigate.nix: updated frigate image version,port mapping and volume mapping

--- hosts/amadeus/services/frigate.nix
+++ hosts/amadeus/services/frigate.nix
@@ -1,14 +1,14 @@
 {
   virtualisation.oci-containers.containers.frigate = {
-    image = "ghcr.io/blakeblackshear/frigate:stable";
+    image = "ghcr.io/blakeblackshear/frigate:0.13.1";
     autoStart = true;
     volumes = [
       "/mnt/cctv:/media/frigate"
-      "/home/tygo/frigate/config.yaml:/config/config.yml:ro"
+      "/home/tygo/frigate:/config"
       "/etc/localtime:/etc/localtime:ro"
     ];
     ports = [
-      "5000:5000"
+      "5001:5000"
       "8554:8554" # RTSP feeds
       "8555:8555/tcp" # WebRTC over tcp
       "8555:8555/udp" # WebRTC over udp
