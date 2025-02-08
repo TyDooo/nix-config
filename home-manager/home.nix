f0983d8ee812a42dc524f687c801ada14ef2fdab
Tygo Driessen <tygo@driessen.family>
2025-02-08 12:55:10 +0000

    Initial Commit

---
+++ home-manager/home.nix
@@ -0,0 +1,67 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use home-manager modules from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModule
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "tygo";
+    homeDirectory = "/home/tygo";
+  };
+
+  home.packages = with pkgs; [
+    (discord-canary.override {
+      withOpenASAR = true;
+    })
+  ];
+
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "tokyonight";
+    };
+  };
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "Tygo Driessen";
+    userEmail = "tygo@driessen.family";
+    aliases = {
+      st = "status";
+    };
+    ignores = [
+      ".direnv"
+      "result"
+    ];
+  };
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "24.11";
+}
