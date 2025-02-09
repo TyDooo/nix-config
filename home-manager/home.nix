60f8cf0d1a746340b3dcf8425f2b0e96e4b04217
Tygo Driessen <tygo@driessen.family>
2025-02-09 14:47:22 +0100

    fix cursor

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -29,6 +29,14 @@
     homeDirectory = "/home/tygo";
   };
 
+  home.pointerCursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
+  };
+
   home.packages = with pkgs; [
     obsidian
     spotify
