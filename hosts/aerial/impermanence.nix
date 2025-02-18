ba64ae74077194b83e1e8f78ba340f3107aabc85
Tygo Driessen <tygo@driessen.family>
2025-02-18 18:35:08 +0100

    Wipe root subvolume using rollback service

--- hosts/aerial/impermanence.nix
+++ hosts/aerial/impermanence.nix
@@ -20,4 +20,35 @@
     };
   };
   programs.fuse.userAllowOther = true;
+
+  boot.initrd.systemd.services.rollback = {
+    description = "Rollback BTRFS root subvolume to a pristine state";
+    wantedBy = ["initrd.target"];
+    after = ["initrd-root-device.target"];
+    before = ["sysroot.mount"];
+    unitConfig.DefaultDependencies = "no";
+    serviceConfig.Type = "oneshot";
+    script = ''
+      mkdir -p /mnt
+
+      # We first mount the btrfs root to /mnt
+      # so we can manipulate btrfs subvolumes.
+      mount -o subvol=/ -t btrfs UUID=a40106bd-3b4f-4970-8d1f-ad1d323131d7 /mnt
+
+      btrfs subvolume list -o "/mnt/root" | cut -f9 -d ' ' |
+      while read subvolume; do
+        echo "deleting /$subvolume subvolume..."
+        btrfs subvolume delete "/mnt/$subvolume"
+      done &&
+      echo "deleting /root subvolume..." &&
+      btrfs subvolume delete /mnt/root
+
+      echo "restoring blank /root subvolume..."
+      btrfs subvolume snapshot /mnt/root-blank /mnt/root
+
+      # Once we're done rolling back to a blank snapshot,
+      # we can unmount /mnt and continue on the boot process.
+      umount /mnt
+    '';
+  };
 }
