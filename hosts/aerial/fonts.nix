3a592b69df095fd93568e43fd838625e1fef5355
Tygo Driessen <tygo@driessen.family>
2025-03-03 16:30:00 +0100

    Update fonts

--- hosts/aerial/fonts.nix
+++ hosts/aerial/fonts.nix
@@ -1,5 +1,51 @@
 {pkgs, ...}: {
-  fonts.packages = with pkgs; [
-    (nerdfonts.override {fonts = ["JetBrainsMono"];})
-  ];
+  fonts = {
+    enableDefaultPackages = false;
+
+    fontconfig = {
+      enable = true;
+      hinting.enable = true;
+      antialias = true;
+    };
+
+    fontDir = {
+      enable = true;
+      decompressFonts = true;
+    };
+
+    packages = with pkgs; [
+      # defaults worth keeping
+      dejavu_fonts
+      freefont_ttf
+      gyre-fonts
+      liberation_ttf # for PDFs, Roman
+      unifont
+      roboto
+
+      # programming fonts
+      sarasa-gothic
+      (nerdfonts.override {fonts = ["JetBrainsMono"];})
+
+      # desktop fonts
+      corefonts # MS fonts
+      b612 # high legibility
+      material-icons # used in widgets and such
+      material-design-icons
+      work-sans
+      comic-neue
+      source-sans
+      inter
+      lato
+      lexend
+      dejavu_fonts
+      noto-fonts
+      noto-fonts-cjk-sans
+
+      # emojis
+      noto-fonts-color-emoji
+      twemoji-color-font
+      openmoji-color
+      openmoji-black
+    ];
+  };
 }
