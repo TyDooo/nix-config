4da4c309b2f02cc4d4694be9caedc6ccc8b94e1b
Tygo Driessen <tygo@driessen.family>
2025-05-27 10:54:18 +0200

    parts: add catastravia to deploy-rs config

--- parts/deploy.nix
+++ parts/deploy.nix
@@ -4,14 +4,31 @@
   ...
 }: {
   flake.deploy = {
+    autoRollback = true;
+    magicRollback = true;
     nodes = {
       zoltraak = {
         hostname = "zoltraak";
+
+        profilesOrder = ["system"];
         profiles.system = {
           sshUser = "tygo";
           user = "root";
           remoteBuild = true;
           path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.zoltraak;
+          interactiveSudo = true;
+        };
+      };
+
+      catastravia = {
+        hostname = "catastravia";
+
+        profilesOrder = ["system"];
+        profiles.system = {
+          sshUser = "tygo";
+          user = "root";
+          remoteBuild = true;
+          path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.catastravia;
           interactiveSudo = true;
         };
       };
