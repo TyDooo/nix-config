0a1e2acc015b28800ed298d422ab088bed5e3f5e
Tygo Driessen <tygo@driessen.family>
2025-06-01 12:48:46 +0200

    users/tygo/home/judradjim: add home packages

--- users/tygo/home/judradjim.nix
+++ users/tygo/home/judradjim.nix
@@ -1,5 +1,17 @@
-{
+{pkgs, ...}: {
   imports = [
     ./common.nix
+  ];
+
+  home.packages = with pkgs; [
+    telegram-desktop
+    obsidian
+    plexamp
+    spotify
+    vesktop
+    logseq
+    picard
+    vscode
+    vlc
   ];
 }
