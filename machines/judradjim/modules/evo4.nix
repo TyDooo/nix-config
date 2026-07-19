b96ebd0f7ead890ea53ba7ce68363dcbcdc164a1
TyDooo <hi@tydooo.dev>
2026-07-19 14:35:50 +0200

    machines/judradjim: remove alsa-ucm-conf override for Audient EVO4 support

--- machines/judradjim/modules/evo4.nix
+++ machines/judradjim/modules/evo4.nix
@@ -1,31 +1,4 @@
-{ pkgs, ... }:
-let
-  # A UCM profile for the EVO4 has been added in v1.2.16, but is not yet
-  # available in nixpkgs unstable. Override the config by setting the
-  # ALSA_CONFIG_UCM2 environment variable.
-  #
-  # FIXME: remove when v1.2.16 or newer is avaliable in nixpkgs
-  alsa-ucm-conf' = pkgs.alsa-ucm-conf.overrideAttrs {
-    src = pkgs.fetchurl {
-      url = "mirror://alsa/lib/alsa-ucm-conf-1.2.16.1.tar.bz2";
-      hash = "sha256-zz0cB+CJqDxOziwg8F3WqKq3/NEIdow4gROGiAV1SSs=";
-    };
-    version = "1.2.16.1";
-    patches = [ ];
-  };
-
-  extraEnv = {
-    ALSA_CONFIG_UCM2 = "${alsa-ucm-conf'}/share/alsa/ucm2";
-  };
-in
 {
-  environment.sessionVariables = extraEnv;
-
-  systemd.services = {
-    pipewire.environment = extraEnv;
-    wireplumber.environment = extraEnv;
-  };
-
   services.pipewire.wireplumber.extraConfig = {
     "9-audient-evo4" = {
       "monitor.alsa.rules" = [
