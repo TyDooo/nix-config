9cbc743fcf7a11c9a6774f0285809dda750fc6bb
TyDooo <hi@tydooo.dev>
2026-08-29 11:07:03 +0200

    users/tydooo: fix fish conditional ssh alias

--- users/tydooo/home/programs/terminal/tools/fish.nix
+++ users/tydooo/home/programs/terminal/tools/fish.nix
@@ -1,5 +1,23 @@
 {
+  osConfig,
+  pkgs,
+  lib,
+  ...
+}:
+{
   programs.fish = {
     enable = true;
+    shellAliases = {
+      cat = "bat";
+      tree = "${pkgs.eza}/bin/eza -T";
+    }
+    //
+      lib.mkIf
+        (builtins.elem "graphical"
+          osConfig.clanConfig.inventory.machines.${osConfig.clan.core.settings.machine.name}.tags
+        )
+        {
+          ssh = "kitten ssh";
+        };
   };
 }
